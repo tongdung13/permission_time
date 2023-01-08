1a1d22640f1e80bc372d953bc3e146485fd654aa
tongdung13 <tongvanthienvu@gmail.com>
2023-01-08 21:57:57 +0700

    update javascript

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/redmond/jquery-ui.css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.js"></script>
+
 </head>
 
 <body>
@@ -22,18 +23,19 @@
         </div>
     @endif
     @yield('content')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js">
-    </script>
-    <script type="text/javascript">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js"> --}}
+    {{-- </script> --}}
+    {{-- <script type="text/javascript">
         $(function() {
-            $('.date').datetimepicker({
+            $('.date').datepicker({
                 format: "DD-MM-YYYY"
             });
         });
-    </script>
+    </script> --}}
+    @yield('script')
 </body>
 
 </html>
--- resources/views/times/create.blade.php
+++ resources/views/times/create.blade.php
@@ -13,9 +13,9 @@
                 <div class="col-6">
                     <div class="form-group">
                         <label for="">Date From</label>
-                        <div class='input-group date' id='datetimepicker'>
-                            <input type="text" name="date_from" class="form-control " value="{{ old('date_from') }}"
-                                id="">
+                        <div class='input-group date'>
+                            <input type="text" name="date_from" class="form-control " id='datepicker'
+                                value="{{ old('date_from') }}" id="">
                             <span class="input-group-addon">
                                 <span class="glyphicon glyphicon-calendar"></span>
                             </span>
@@ -24,9 +24,9 @@
                 </div>
                 <div class="col-6">
                     <label for="">From To</label>
-                    <div class='input-group date' id='datetimepicker'>
-                        <input type="text" name="date_to" class="form-control " value="{{ old('date_to') }}"
-                            id="">
+                    <div class='input-group date'>
+                        <input type="text" name="date_to" class="form-control " id='datepicker1'
+                            value="{{ old('date_to') }}" id="">
                         <span class="input-group-addon">
                             <span class="glyphicon glyphicon-calendar"></span>
                         </span>
@@ -41,5 +41,39 @@
         </form>
     </div>
 
+
+@endsection
+
+@section('script')
+
+    <script>
+        $(document).ready(function() {
+            $("#datepicker").datepicker({
+                dateFormat: "dd-m-yy",
+                // showMonthAfterYear: true,
+
+                numberOfMonths: 1,
+                minDate: 0,
+                // onSelect: function(selected) {
+                //     $("#datepicker1").datepicker("option", "minDate", selected);
+                //     select();
+                // }
+            });
+            $("#datepicker1").datepicker({
+                dateFormat: "dd-m-yy",
+                minDate: 1,
+                onClose: function() {
+                    var dt1 = $('#datepicker').datepicker('getDate');
+                    var dt2 = $('#datepicker1').datepicker('getDate');
+                    if (dt2 <= dt1) {
+                        var minDate = $('#datepicker1').datepicker('option', 'minDate');
+                        $('#datepicker1').datepicker('setDate', minDate);
+                    }
+                }
+            }).datepicker("datepicker", new Date());
+
+
+        });
+    </script>
 
 @endsection
